c517ebc5db639b419ac2017219879b8416a3e680
Artur Olenberg <olenberg.ai@gmail.com>
2023-02-23 19:37:38 +0600

    homework-23

---
+++ main.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, abort
+from pathlib import Path
+from request_handler import RequestHandler
+
+
+app = Flask(__name__)
+DATA_DIR = Path.cwd() / "data"
+
+
+@app.post("/perform_query")
+def perform_query():
+    file_name = request.args.get("file_name")
+    cmd1 = request.args.get("cmd1")
+    value1 = request.args.get("value1")
+    cmd2 = request.args.get("cmd2")
+    value2 = request.args.get("value2")
+
+    if None in (cmd1, value1, cmd2, value2):
+        abort(400, "Incorrect parameters passed")
+
+    if not Path.exists(DATA_DIR / file_name):
+        abort(400, "File not found")
+
+    try:
+        with open(DATA_DIR / file_name, "r", encoding="utf-8") as f:
+            first_result = getattr(RequestHandler, cmd1)(f, value1)
+            second_result = getattr(RequestHandler, cmd2)(first_result, value2)
+    except (TypeError, ValueError) as e:
+        abort(400, e)
+
+    return app.response_class(second_result, content_type="text/plain")
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000, debug=True)
---
+++ request_handler.py
@@ -0,0 +1,33 @@
+from typing import Iterator, Iterable, Union
+
+
+class RequestHandler:
+    @staticmethod
+    def filter(iterable: Iterator, value: str) -> Iterable:
+        if not isinstance(value, str):
+            raise TypeError
+        return filter(lambda line: value in line, iterable)
+
+    @staticmethod
+    def sort(iterable: Iterator, order: str = "asc") -> list:
+        if not order in ["asc", "desc"]:
+            raise ValueError
+        if order == "desc":
+            return sorted(iterable, reverse=True)
+        return sorted(iterable)
+
+    @staticmethod
+    def map(iterable: Iterator, column: Union[str, int]) -> Iterable:
+        if not str(column).isdigit():
+            raise TypeError
+        return map(lambda line: line.split(" ")[int(column)] + "\n", iterable)
+
+    @staticmethod
+    def limit(iterable: Iterator, number: Union[str, int]) -> list:
+        if not str(number).isdigit():
+            raise TypeError
+        return list(iterable)[:int(number)]
+
+    @staticmethod
+    def unique(iterable: Iterator, value="") -> set:
+        return set(iterable)
